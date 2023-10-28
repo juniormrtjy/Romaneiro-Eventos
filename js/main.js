65c132730f3643d5a79724ea277489a7c7750ab3
Paulo Roberto <paulorobertomrtjy@gmail.com>
2023-10-27 21:16:18 -0300

    ajustando o menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
-const menu = document.querySelector('.main-header')
+const mainHeader = document.querySelector('.main-header')
+const nav = document.querySelector('.nav')
+const openMenu = document.querySelector('.open-menu')
+const closeMenu = document.querySelector('.close-menu')
 
 document.addEventListener('scroll', () => {
   if (window.scrollY > 600) {
-    menu.style.position = 'sticky'
+    mainHeader.style.position = 'sticky'
   } else {
-    menu.style.position = 'static'
+    mainHeader.style.position = 'static'
   }
+})
+
+setInterval(() => {
+  if (window.innerWidth < 1130) {
+    nav.style.display = 'none'
+    openMenu.style.display = 'block'
+    closeMenu.style.display = 'block'
+  } else {
+    nav.style.display = 'flex'
+
+    openMenu.style.display = 'none'
+    closeMenu.style.display = 'none'
+  }
+}, 50)
+
+function toggleMenu() {
+  nav.classList.toggle('show')
+}
+
+const menuBtn = document.querySelectorAll('.toggle')
+
+menuBtn.forEach(link => {
+  link.addEventListener('click', toggleMenu)
 })
